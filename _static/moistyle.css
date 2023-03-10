a24b060cc6ce771e8c5e91c9897acb6cbb9e51f8
Moisès Gómez Girón <moisesgomezgiron@gmail.com>
2023-03-10 10:20:36 +0100

    automated commit on divendres, 10 de març de 2023, 10:20:36 CET

---
+++ _static/moistyle.css
@@ -0,0 +1,41 @@
+/*
+* Functions to deal with cookies acceptance
+*/
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+} 
+
+function setUserAcceptsCookies(){
+    setCookie("apuntstecnics", "accepted", 10000);
+    setCookiesboxVisibility();
+}
+ 
+function setCookiesboxVisibility() {
+    var cookiesbox = document.getElementById("cookiesbox");
+    if (getCookie("apuntstecnics") == "") {
+        cookiesbox.style.display = "block";
+    } else {
+        cookiesbox.style.display = "none";
+    }
+}
+
